6f44a0b0478f0cbafad83eb58e3c5ab591e99378
Gaurav <gauravdey625@gmail.com>
2021-04-14 13:14:18 +0530

    Changed video api and icon

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const config ={
-    YOUTUBE_LINK:'https://www.googleapis.com/youtube/v3/videos?part=snippet&part=statistics&chart=mostPopular&maxResults=50',
+    YOUTUBE_LINK:'https://www.googleapis.com/youtube/v3/videos?part=snippet&part=statistics&chart=mostPopular&regionCode=IN&maxResults=50',
     API_KEY:'AIzaSyA3dgnhBYnDQG6JA_bqCO39B-i1DvyC1uQ'
 }
