3355a725eadfce00187b3ba2c17d6d0755727bb1
Gaurav <gauravdey625@gmail.com>
2021-05-31 21:08:49 +0530

    Added scroll to top button

--- src/pages/AllVideos.js
+++ src/pages/AllVideos.js
@@ -1,49 +1,53 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { useVideoContext } from '../Context/VideoContext';
-import { LazyLoadImage } from 'react-lazy-load-image-component'
-import 'react-lazy-load-image-component/src/effects/blur.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import { useVideoContext } from "../Context/VideoContext";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
+import ScrollToTop from "../Components/ScrollToTop";
 function ViewCalculator({ views }) {
-    if (views > 1000000)
-        return Math.round(views * 100 / 1000000) / 100 + 'M views'
-    else
-        return Math.round(views * 100 / 1000) / 100 + 'k views'
+  if (views > 1000000)
+    return Math.round((views * 100) / 1000000) / 100 + "M views";
+  else return Math.round((views * 100) / 1000) / 100 + "k views";
 }
 function DateCalculator({ date }) {
-    let timeElapsed = Date.now() - Date.parse(date);
-    let timeElapsed_inhours = Math.ceil(timeElapsed / 3600000);
-    if (timeElapsed_inhours < 23)
-        return timeElapsed_inhours + ' hours ago'
-    else if (timeElapsed_inhours === 24)
-        return Math.ceil(timeElapsed_inhours / 24) + ' day ago'
-    else
-        return Math.ceil(timeElapsed_inhours / 24) + ' days ago'
+  let timeElapsed = Date.now() - Date.parse(date);
+  let timeElapsed_inhours = Math.ceil(timeElapsed / 3600000);
+  if (timeElapsed_inhours < 23) return timeElapsed_inhours + " hours ago";
+  else if (timeElapsed_inhours === 24)
+    return Math.ceil(timeElapsed_inhours / 24) + " day ago";
+  else return Math.ceil(timeElapsed_inhours / 24) + " days ago";
 }
 export function AllVideosListing() {
-    const { AllVideos } = useVideoContext();
-    return (
-        <div className="AllVideos">
-            { AllVideos.map(video => {
-                return (
-                    <div key={video.id} >
-                        <Link to={`/video/${video.id}`} style={{ textDecoration: "none" }}>
-                            <div className="videoCard" >
-                                <LazyLoadImage
-                                    src={video.snippet.thumbnails.medium.url}
-                                    alt="Loading...."
-                                    effect="blur"
-                                    width="300px" />
-                                <div className="box-body">
-                                    <p style={{ textAlign: "start" }}>{video.snippet.title}</p>
-                                    <p style={{ marginTop: "10px", textAlign: "start" }}>{video.snippet.channelTitle}</p>
-                                    <p style={{ textAlign: "start" }}>{<ViewCalculator views={video.statistics.viewCount} />} . {<DateCalculator date={video.snippet.publishedAt} />}</p>
-                                </div>
-                            </div>
-                        </Link>
-                    </div>
-                )
-            })
-            }
-        </div>
-    )
+  const { AllVideos } = useVideoContext();
+  return (
+    <div className="AllVideos">
+      <ScrollToTop />
+      {AllVideos.map((video) => {
+        return (
+          <div key={video.id}>
+            <Link to={`/video/${video.id}`} style={{ textDecoration: "none" }}>
+              <div className="videoCard">
+                <LazyLoadImage
+                  src={video.snippet.thumbnails.medium.url}
+                  alt="Loading...."
+                  effect="blur"
+                  width="300px"
+                />
+                <div className="box-body">
+                  <p style={{ textAlign: "start" }}>{video.snippet.title}</p>
+                  <p style={{ marginTop: "10px", textAlign: "start" }}>
+                    {video.snippet.channelTitle}
+                  </p>
+                  <p style={{ textAlign: "start" }}>
+                    {<ViewCalculator views={video.statistics.viewCount} />} .{" "}
+                    {<DateCalculator date={video.snippet.publishedAt} />}
+                  </p>
+                </div>
+              </div>
+            </Link>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
